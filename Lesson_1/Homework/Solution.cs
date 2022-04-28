cd419e667fb3323d9d470aba375917a88e2b632c
NaumenkoAl <inaumenkoalecsey@mail.ru>
2022-04-28 12:00:59 +0300

    Lesson_1

---
+++ Lesson_1/Homework/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            DateTime myDate = DateTime.Now;
+            Console.WriteLine("Введите Ваше Имя: ");
+            string inputName = Console.ReadLine();  
+            Console.WriteLine($"Привет, {inputName}, сегодня {myDate}");
+            Console.ReadLine();
+        }
+    }
+}
